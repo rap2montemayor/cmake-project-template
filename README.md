6efcf855becaaae38e0f7fdc34290022c0b03419
rap2montemayor <rap2montemayor@gmail.com>
2024-05-08 07:20:29 +0800

    Made a minimal version

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # cmake-project-template
 
-Loosely based on the [Canonical Project Structure](https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p1204r0.html)
-
 To compile:
 
-`cmake -B build -S . -G "Ninja"`
-
-`cmake --build build --parallel 8`
+```
+cmake -B build -S . -G "Ninja"
+cmake --build build --parallel 8
+```
--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,33 +1,33 @@
 include(FetchContent)
 
 # Catch2::Catch2WithMain
-FetchContent_Declare(
-  Catch2
-  GIT_REPOSITORY "https://github.com/catchorg/Catch2.git"
-  GIT_TAG b5373dadca40b7edc8570cf9470b9b1cb1934d40  # v3.5.4
-)
-FetchContent_MakeAvailable(Catch2)
-list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
-include(CTest)
-include(Catch)
+# FetchContent_Declare(
+#   Catch2
+#   GIT_REPOSITORY "https://github.com/catchorg/Catch2.git"
+#   GIT_TAG b5373dadca40b7edc8570cf9470b9b1cb1934d40  # v3.5.4
+# )
+# FetchContent_MakeAvailable(Catch2)
+# list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
+# include(CTest)
+# include(Catch)
 
 
 # Boost
-set (BOOST_LIBS config container_hash describe mp11)
-foreach(BOOST_LIB IN LISTS BOOST_LIBS)
-  FetchContent_Declare(
-    boost_${BOOST_LIB}
-    GIT_REPOSITORY "https://github.com/boostorg/${BOOST_LIB}.git"
-    GIT_TAG boost-1.84.0
-  )
-  FetchContent_MakeAvailable(boost_${BOOST_LIB})
-endforeach()
+# set (BOOST_LIBS config container_hash describe mp11)
+# foreach(BOOST_LIB IN LISTS BOOST_LIBS)
+#   FetchContent_Declare(
+#     boost_${BOOST_LIB}
+#     GIT_REPOSITORY "https://github.com/boostorg/${BOOST_LIB}.git"
+#     GIT_TAG boost-1.84.0
+#   )
+#   FetchContent_MakeAvailable(boost_${BOOST_LIB})
+# endforeach()
 
 
 # spdlog
 FetchContent_Declare(
   spdlog
   GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
-  GIT_TAG v1.13.0
+  GIT_TAG 27cb4c76708608465c413f6d0e6b8d99a4d84302  # v1.14.1
 )
 FetchContent_MakeAvailable(spdlog)
